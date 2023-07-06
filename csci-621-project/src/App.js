8290fd405bc65f574b4e7eb26909973b36654b2c
chinmay13 <chinmaybhate123@gmail.com>
2023-07-05 19:15:51 -0500

    Read from firebase

--- csci-621-project/src/App.js
+++ csci-621-project/src/App.js
@@ -1,8 +1,57 @@
-import TODO from "./Todo";
+import React, { useEffect, useState } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import Todo from "./Todo";
+import { db } from "./Backend/Firebase_Init";
+import { query, collection, onSnapshot } from "firebase/firestore";
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto shadow-xl  p-4`,
+  heading: `text-3xl font-bold text-center text-grey-800 p-2`,
+  form: `flex hustify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-purple-400 text-slate-100`,
+  count: `text-center p-2`,
+};
+
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  //create todo
+  //read todo
+  useEffect(() => {
+    const q = query(collection(db, "todos"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let todosArr = [];
+      querySnapshot.forEach((doc) => {
+        todosArr.push({ ...doc.data(), id: doc.id });
+      });
+      setTodos(todosArr);
+    });
+    return () => unsubscribe();
+  }, []);
+  //update todo in firebase
+  //delete todo
+
   return (
-    <div className="App">
-      <TODO></TODO>
+    <div className={style.bg}>
+      <div>
+        <div className={style.container}>
+          <h3 className={style.heading}>Todo App</h3>
+          <form className={style.form}>
+            <input type="text" className={style.input} placeholder="Add Todo" />
+            <button className={style.button}>
+              <AiOutlinePlus size={30} />
+            </button>
+          </form>
+          <ul>
+            {todos.map((todo, index) => (
+              <Todo key={index} todo={todo} />
+            ))}
+          </ul>
+          <p className={style.count}>You have 2 todos</p>
+        </div>
+      </div>
     </div>
   );
 }
--- csci-621-project/src/Backend/Firebase_Init.js
+++ csci-621-project/src/Backend/Firebase_Init.js
@@ -1,27 +1,19 @@
 // Import the functions you need from the SDKs you need
-//import { initializeApp } from "firebase/app";
-//import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
-import { getAnalytics } from "firebase/analytics";
-import firebase from "firebase";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAJiPxri_NhpfhSjhO-3XSlW7oaWNaz0dY",
-  authDomain: "csci-621-project-fb6a2.firebaseapp.com",
-  projectId: "csci-621-project-fb6a2",
-  storageBucket: "csci-621-project-fb6a2.appspot.com",
-  messagingSenderId: "676915185863",
-  appId: "1:676915185863:web:1512f6e1c5834dd7b016e2",
-  measurementId: "G-4PDHW67XV0",
+  apiKey: "AIzaSyDKM10FbkVn9tZxB2Oh_PJW17Toc-v99XQ",
+  authDomain: "todo-app-73c32.firebaseapp.com",
+  projectId: "todo-app-73c32",
+  storageBucket: "todo-app-73c32.appspot.com",
+  messagingSenderId: "1059441317369",
+  appId: "1:1059441317369:web:69798210e7b96ce3499928",
 };
 
 // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-const firebaseDB = app.firestore();
-
-export default firebaseDB;
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
